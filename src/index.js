f0fc991e81eda1e64ae886d81716f6044348391e
Chris Wood <wchristopherwood912@gmail.com>
2024-02-13 15:06:23 -0800

    add query client and redux

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+// Add React Query
+import { QueryClientProvider, QueryClient } from "react-query";
+// Add Redux
+import { store } from "./app/store";
+import { Provider } from "react-redux";
+// Create a QueryClient
+const queryClient = new QueryClient();
+// Wrap the App in the QueryClientProvider
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
